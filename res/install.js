4331fa0cabd8d4dea63d9ba0ef205d0fdd508241
程龙 <cangsangzhinian@163.com>
2017-07-21 10:46:56 +0800

    add

--- res/install.js
+++
@@ -1,4 +0,0 @@
-function openInstallURL() {
-  var url = 'itms-services://?action=download-manifest&url=https://github.com/feitiandalong/ServiceApi/blob/master/res/install.plist';
-  window.self.location = url;
-}
--- safety/install.js
+++
@@ -1,4 +0,0 @@
-function openInstallURL() {
-  var url = 'itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/feitiandalong/ServiceApi/master/res/install.plist';
-  window.self.location = url;
-}
